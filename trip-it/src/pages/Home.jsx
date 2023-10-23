abda04be57f3058405d9a59ca67360af3f4dd235
syafrudinhl <syafrudintdm@gmail.com>
2023-10-24 02:21:11 +0700

    add SVG

--- trip-it/src/pages/Home.jsx
+++ trip-it/src/pages/Home.jsx
@@ -5,6 +5,7 @@ import Img from "../assets/images/hero-img01.jpg";
 import Img2 from "../assets/images/hero-img02.jpg";
 import Video from "../assets/images/video.mp4";
 import worldImg from "../assets/images/world.png";
+import experienceImg from "../assets/images/experience.svg";
 import Subtitle from "../shared/Subtitle";
 
 import ServiceList from "../services/ServiceList";
@@ -80,9 +81,55 @@ export default function Home() {
           <Row>
             <Col lg="12" className="mb-5">
               <Subtitle subtitle={"Explore"}></Subtitle>
-              <h2 className="featured__tour-title">Tour unggulan kami</h2>
+              <h2 className="featured__tour-title">
+                Tour unggulan kami
+                <br />
+              </h2>
             </Col>
             <FeaturedTourList></FeaturedTourList>
+          </Row>
+        </Container>
+      </section>
+
+      <section>
+        <Container>
+          <Row className="align-items-center">
+            <Col lg="6">
+              <div className="experience__content">
+                <Subtitle subtitle={"Pengalaman"}></Subtitle>
+                <h2>
+                  Dengan semua pengalaman kami
+                  <br />
+                  kami akan melayani anda
+                </h2>
+                <p>
+                  Kami adalah tim profesional yang siap membuat perjalanan Anda
+                  tak terlupakan. Dengan pengetahuan mendalam, kami pandu Anda
+                  melintasi destinasi eksotis. Keindahan dunia, detail tak
+                  terlewatkan, pengalaman tak terlupakan.
+                </p>
+              </div>
+
+              <div className="counter__wrapper d-flex align-items-center gap-5">
+                <div className="counter__box">
+                  <span>12k+</span>
+                  <h6>Perjalanan yang sukses</h6>
+                </div>
+                <div className="counter__box">
+                  <span>2k+</span>
+                  <h6>Kliens reguler</h6>
+                </div>
+                <div className="counter__box">
+                  <span>15</span>
+                  <h6>Tahun pengalaman</h6>
+                </div>
+              </div>
+            </Col>
+            <Col lg="6">
+              <div className="experience__img ms-5">
+                <img src={experienceImg} alt="" />
+              </div>
+            </Col>
           </Row>
         </Container>
       </section>
