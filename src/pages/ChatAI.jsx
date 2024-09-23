2ac8321d817d457f905af91ec3e07121d9650c78
syafrudinhl <syafrudintdm@gmail.com>
2024-09-23 23:11:44 +0700

    Deleted OpenAI token

--- src/pages/ChatAI.jsx
+++ src/pages/ChatAI.jsx
@@ -5,7 +5,7 @@ import avatar from "../assets/images/avatar.jpg";
 
 export default function ChatAI() {
   const openai = new OpenAI({
-    apiKey: process.env.REACT_APP_API_KEY,
+    apiKey: "",
     dangerouslyAllowBrowser: true,
   });
 
